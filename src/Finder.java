cb4bdeddf2db71a2ad40a7bab81ab7bb1dc505a1
sierrashaw <sierra.shaw@menloschool.org>
2024-11-05 21:45:38 -0800

    Debugging - failed.

--- src/Finder.java
+++ src/Finder.java
@@ -102,45 +102,4 @@ public class Finder {
             tempKeys[idx] = pair;
         }
     }
-
-    /*
-    public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
-        String line;
-        String key;
-        String value;
-        int firstHash;
-        int secondHash;
-        while((line = br.readLine()) != null) {
-            String[] splitLine = line.split(",");
-            key = splitLine[keyCol];
-            value = splitLine[valCol];
-
-            firstHash = hash(key, key.length(), first_p);
-            secondHash = hash(key, key.length(), second_p);
-            if (keys[firstHash] == null) {
-                keys[firstHash] = new ArrayList<Pair>();
-            }
-
-            Pair pair = new Pair(secondHash, value);
-            keys[firstHash].add(pair);
-        }
-        br.close();
-    }
-
-    public String query(String key){
-        int keyLen = key.length();
-        int firstHash = hash(key, keyLen, first_p);
-        if (keys[firstHash] == null) {
-            return INVALID;
-        }
-        ArrayList<Pair> toSearch = keys[firstHash];
-        int secondHash = hash(key, keyLen, second_p);
-        for (int i = 0; i < toSearch.size(); i++) {
-            if (toSearch.get(i).key == secondHash) {
-                return toSearch.get(i).val;
-            }
-        }
-        return INVALID;
-    }
-     */
 }
